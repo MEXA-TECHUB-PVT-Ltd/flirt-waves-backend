d881e00c01e6aabd868270a456f465aa9101108a
RimshaRiazMtechub <rimshariaz@mtechub.org>
2023-11-29 17:05:19 +0500

    flirt waves 29-11-2023

--- app/routes/crush/crushroutes.js
+++ app/routes/crush/crushroutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const crushController = require("../../controllers/crushcontroller");
 
 router.post('/add/:userId', crushController.addcrush); 
-router.get('/getusercrushes/:userId', crushController.getUserCrushes);  
+router.get('/getusercrushes/:userId', crushController.getUserCrushes);
+router.delete('/remove_user_crush/:userId', crushController.deleteuserCrush);
+router.delete('/delete_AllUser_Crushes/:userId', crushController.deleteAllUserCrushes); 
+router.get('/get_All_Crushes', crushController.getAllCrushesWithUsers);  
+router.put('/update_user_Crushes/:userId ', crushController.updateCrushes);      
 
 module.exports = router;
